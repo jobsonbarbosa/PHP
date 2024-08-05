521e74ac62ded9cd62de74d03a8924916576fb3d
jobsonbarbosa <jobsonbarbosa86@gmail.com>
2024-08-04 21:04:50 -0300

    criação da classe Usuário

--- Udemy/dao/Usuario.php
+++ Udemy/dao/Usuario.php
@@ -1,1 +1,71 @@
 <?php
+
+class Usuario {
+
+    private $idusuario;
+    private $deslogin;
+    private $dessenha;
+    private $dtcadastro;
+
+    //id usuário
+    public function getIdusuario() {
+        return $this->idusuario;
+    }
+    public function setIdusuario($value) {
+        $this->idusuario = $value;
+    }
+
+        //deslogin
+    public function getDeslogin() {
+        return $this->deslogin;
+    } 
+    public function setDeslogin($value) {
+        $this->deslogin = $value;
+    }
+
+    //dessenha
+    public function getDessenha() {
+        return $this->dessenha;
+    }
+    public function setDessenha($value) {
+        $this->dessenha = $value;
+    }
+
+    //dessenha
+    public function getDtcadastro() {
+        return $this->dtcadastro;
+    }
+    public function setDtcadastro($value) {
+        $this->dtcadastro = $value;
+    }
+
+    public function loadByID($id) {
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+            ":ID"=>$id
+        ));
+
+        if(count($results) > 0 ) {
+
+            $row = $results[0];
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+        }
+    }
+
+    public function __tostring() {
+
+        return json_encode(array(
+            "idusuario"=> $this->getIdusuario(),
+            "deslogin"=> $this->getDeslogin(),
+            "dessenha"=> $this->getDessenha(),
+            "dtcadastro"=> $this->getDtcadastro()->format("d/m/Y H:i:s")
+
+        ));
+    }
+
+}
--- Udemy/dao/index.php
+++ Udemy/dao/index.php
@@ -1,9 +1,20 @@
 <?php
 
 require_once("config.php");
+require_once("/xampp/htdocs/Udemy/dao/Usuario.php");
 
+$usuario = new Usuario();
+
+$usuario->loadByID(2);
+
+echo($usuario);
+
+
+/*
+//consulta de todos os usuário
 $sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
+*/
